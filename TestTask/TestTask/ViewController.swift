05a143368e7ba1ba0c22f62cbcd52afd24aaba2d
Boris Chechko <boris.chechko@mdcm.co>
2020-11-22 11:51:15 +0400

    root

---
+++ TestTask/TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by  Boris Chechko on 22.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
